6689d7b3a8dabb805375a7508025c0c465756fcd
Akbar Abayev <105016091+MedicalEnvironment@users.noreply.github.com>
2023-06-19 01:17:36 +0300

    Update main.cpp

--- Elf Villag/main.cpp
+++ Elf Villag/main.cpp
@@ -83,29 +83,94 @@ void placeElves(Branch* branch) {
 
 int main() {
     // Generate the forest structure
-    std::vector<Branch> tree(5);
-    tree[0].addChildBranch(&tree[1]);
-    tree[0].addChildBranch(&tree[2]);
-    tree[0].addChildBranch(&tree[3]);
-    tree[1].addChildBranch(&tree[4]);
-    tree[2].addChildBranch(&tree[4]);
-    tree[3].addChildBranch(&tree[4]);
+    std::vector<Branch> trees(5);
+
+    // Tree 1
+    Branch* tree1Branch1 = new Branch();
+    Branch* tree1Branch2 = new Branch();
+    Branch* tree1Branch3 = new Branch();
+    trees[0].addChildBranch(tree1Branch1);
+    trees[0].addChildBranch(tree1Branch2);
+    trees[0].addChildBranch(tree1Branch3);
+
+    // Tree 2
+    Branch* tree2Branch1 = new Branch();
+    Branch* tree2Branch2 = new Branch();
+    Branch* tree2Branch3 = new Branch();
+    Branch* tree2Branch4 = new Branch();
+    trees[1].addChildBranch(tree2Branch1);
+    trees[1].addChildBranch(tree2Branch2);
+    trees[1].addChildBranch(tree2Branch3);
+    trees[1].addChildBranch(tree2Branch4);
+
+    // Tree 3
+    Branch* tree3Branch1 = new Branch();
+    Branch* tree3Branch2 = new Branch();
+    Branch* tree3Branch3 = new Branch();
+    trees[2].addChildBranch(tree3Branch1);
+    trees[2].addChildBranch(tree3Branch2);
+    trees[2].addChildBranch(tree3Branch3);
+
+    // Tree 4
+    Branch* tree4Branch1 = new Branch();
+    Branch* tree4Branch2 = new Branch();
+    trees[3].addChildBranch(tree4Branch1);
+    trees[3].addChildBranch(tree4Branch2);
+
+    // Tree 5
+    Branch* tree5Branch1 = new Branch();
+    Branch* tree5Branch2 = new Branch();
+    trees[4].addChildBranch(tree5Branch1);
+    trees[4].addChildBranch(tree5Branch2);
+
+    // Set parent branches
+    tree1Branch1->setParentBranch(&trees[0]);
+    tree1Branch2->setParentBranch(&trees[0]);
+    tree1Branch3->setParentBranch(&trees[0]);
+
+    tree2Branch1->setParentBranch(&trees[1]);
+    tree2Branch2->setParentBranch(&trees[1]);
+    tree2Branch3->setParentBranch(&trees[1]);
+    tree2Branch4->setParentBranch(&trees[1]);
+
+    tree3Branch1->setParentBranch(&trees[2]);
+    tree3Branch2->setParentBranch(&trees[2]);
+    tree3Branch3->setParentBranch(&trees[2]);
+
+    tree4Branch1->setParentBranch(&trees[3]);
+    tree4Branch2->setParentBranch(&trees[3]);
+
+    tree5Branch1->setParentBranch(&trees[4]);
+    tree5Branch2->setParentBranch(&trees[4]);
 
     // Place the elves in the houses
-    placeElves(&tree[0]);
+    for (auto& tree : trees) {
+        placeElves(&tree);
+    }
 
     // Search for a specific elf
     std::string searchName;
     std::cout << "Enter the name of the elf you are looking for: ";
     std::cin >> searchName;
 
-    Branch* foundElf = tree[0].searchElf(searchName);
+    Branch* foundElf = nullptr;
+    for (auto& tree : trees) {
+        foundElf = tree.searchElf(searchName);
+        if (foundElf != nullptr)
+            break;
+    }
 
     if (foundElf != nullptr) {
         int totalNeighbors = foundElf->getTotalNeighbors();
         std::cout << "The total number of neighbors for " << searchName << " is: " << totalNeighbors << std::endl;
     } else {
         std::cout << "Elf not found!" << std::endl;
+    }
+
+    // Clean up memory
+    for (auto& tree : trees) {
+        delete tree.getChildBranches()[0];
+        delete tree.getChildBranches()[1];
     }
 
     return 0;
